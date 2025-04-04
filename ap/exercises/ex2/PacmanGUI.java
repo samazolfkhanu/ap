1c42961a421913fa1aa452f40f52d10d56903242
samazolfkhanu <samazolfkhani0@gmail.com>
2025-04-03 23:49:43 -0700

    EX2_PM_3_1_B

--- ap/exercises/ex2/PacmanGUI.java
+++ ap/exercises/ex2/PacmanGUI.java
@@ -118,15 +118,31 @@ public class PacmanGUI extends JFrame implements KeyListener {
         System.out.println("direction:" + direction + "    <- e.getKeyCode()=" + e.getKeyCode());
 
         repaint();
-        
+
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
     }
 
-    private void handleCrossBorder(){
-        //
+    private void handleCrossBorder()
+    {
+        if(pacmanPoint.x<0)
+        {
+            pacmanPoint.x=(width/boxSize)-1;
+        }
+        else if(pacmanPoint.x>=(width/boxSize))
+        {
+            pacmanPoint.x=0;
+        }
+        if(pacmanPoint.y<0)
+        {
+            pacmanPoint.y=(height/boxSize)-1;
+        }
+        else if(pacmanPoint.y>=(height/boxSize))
+        {
+            pacmanPoint.y=0;
+        }
     }
 
 
