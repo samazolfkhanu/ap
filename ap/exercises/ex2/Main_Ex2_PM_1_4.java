ed9210ce36077750fea4517b809ab63650407ded
samazolfkhanu <samazolfkhani0@gmail.com>
2025-04-08 02:20:51 -0700

    EX2_PM_1_4

--- ap/exercises/ex2/Main_Ex2_PM_1_4.java
+++ ap/exercises/ex2/Main_Ex2_PM_1_4.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main_Ex2_PM_1_4
 {
-    public void main(String[] args)
+    public static void main(String[] args)
     {
         Scanner s=new Scanner(System.in);
         String c;
