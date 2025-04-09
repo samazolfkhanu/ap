c01bd29ae301b0e3e0710f0aee5218995b12b460
samazolfkhanu <samazolfkhani0@gmail.com>
2025-04-09 01:40:01 -0700

    EX3_LM_1_1

---
+++ ap/exercises/ex3/Book.java
@@ -0,0 +1,77 @@
+package ap.exercises.ex3;
+
+public class Book
+{
+    private String bN;
+    private String aN;
+    private int nP;
+    private int pY;
+    public Book(String bN,String aN,int nP,int pY)throws InvalidInputException
+    {
+        if(bN!=null && aN!=null && nP>0 && pY>0)
+        {
+            this.bN = bN;
+            this.aN = aN;
+            this.nP = nP;
+            this.pY = pY;
+        }
+        else {
+            throw new InvalidInputException("Error: invalid input!");
+        }
+    }
+
+    public String getBookName()
+    {
+        return bN;
+    }
+    public void setBookName(String bN) throws InvalidInputException
+    {
+        if(bN!=null)
+            this.bN=bN;
+        else
+            throw new InvalidInputException("invalid input!");
+    }
+
+    public String getAuthorName()
+    {
+        return aN;
+    }
+    public void setAuthorName(String aN) throws InvalidInputException
+    {
+        if(aN!=null)
+            this.aN=aN;
+        else
+            throw new InvalidInputException("invalid input!");
+    }
+
+    public int getNumPage()
+    {
+        return nP;
+    }
+    public void setNumPage(int nP)throws InvalidInputException
+    {
+        if(nP>0)
+            this.nP=nP;
+        else
+            throw new InvalidInputException("invalid input!");
+    }
+
+    public int getPublishedYear()
+    {
+        return pY;
+    }
+    public void setPublishedYear(int pY) throws InvalidInputException
+    {
+        if(pY>0)
+            this.pY=pY;
+        else
+            throw new InvalidInputException("invalid input!");
+
+    }
+
+    @Override
+    public String toString()
+    {
+        return "book name: "+this.bN+"\tauthor name: "+aN+"\tnumber of pages: "+nP+"\tyear of published:"+pY+"\n";
+    }
+}
