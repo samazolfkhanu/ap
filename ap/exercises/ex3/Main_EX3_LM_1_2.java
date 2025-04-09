a45b61bdfe048984cb943f213ff6367fc5821272
samazolfkhanu <samazolfkhani0@gmail.com>
2025-04-09 02:00:29 -0700

    EX3_LM_1_2_A

--- ap/exercises/ex3/Main_EX3_LM_1_2.java
+++ ap/exercises/ex3/Main_EX3_LM_1_2.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main_EX3_LM_1_2
 {
-    public static void main(String[] args) throws InvalidInputExceptionErr {
+    public static void main(String[] args)  {
         Scanner s = new Scanner(System.in);
         int c, c1, c2;
         do {
@@ -14,7 +14,7 @@ public class Main_EX3_LM_1_2
             switch (c) {
                 case 1:
                     do {
-                        System.out.println("1.add\n2.display info\n3.exit");
+                        System.out.println("1.add\n2.exit\n");
                         c1 = s.nextInt();
                         s.nextLine();
                         switch (c1) {
@@ -22,7 +22,7 @@ public class Main_EX3_LM_1_2
                                 System.out.println("enter number of books: ");
                                 int nb = s.nextInt();
                                 s.nextLine();
-                                BookLib[] b = new BookLib[nb];
+                                Book[] b = new Book[nb];
                                 for (int i = 0; i < nb; i++) {
                                     System.out.println("enter name of book,name of author,number of pages and published year:");
                                     String bN = s.nextLine();
@@ -30,25 +30,46 @@ public class Main_EX3_LM_1_2
                                     int nP = s.nextInt();
                                     int pY = s.nextInt();
                                     s.nextLine();
-                                    b[i] = new BookLib(bN, aN, nP, pY);
-                                    FileHandle.writeInFile(b[i], "F:/MainProjects/ap/exercises/ex3/Book.txt");
+                                    try
+                                    {
+                                        b[i] = new Book(bN, aN, nP, pY);
+                                    }
+                                    catch(InvalidInputException e)
+                                    {
+                                        System.out.println(e.getMessage());
+                                    }
+                                    try
+                                    {
+                                        File f=new File("F:/MainProjects/ap/exercises/ex3/Book.txt");
+                                        ObjectOutputStream O;
+                                        if(f.length()==0)
+                                        {
+                                            O=new ObjectOutputStream(new FileOutputStream("F:/MainProjects/ap/exercises/ex3/Book.txt"));
+                                            O.writeObject(b[i]);
+                                        }
+                                        else
+                                        {
+                                            O=new AppendObj(new FileOutputStream("F:/MainProjects/ap/exercises/ex3/Book.txt",true));
+                                            O.writeObject(b[i]);
+                                        }
+                                    }
+                                    catch(Exception e)
+                                    {
+                                        System.out.println(e.getMessage());
+                                    }
                                 }
                                 break;
 
                             case 2:
-                                FileHandle.readFileB("F:/MainProjects/ap/exercises/ex3/Book.txt");
-                                break;
-
-                            case 3:
                                 System.out.println("exiting...");
 
                         }
-                    } while (c1 != 3);
+                    } while (c1 != 2);
                     break;
 
                 case 2:
                     do {
-                        System.out.println("1.add\n2.display info\n3.exit\n");
+                        System.out.println("1.add\n2.exit\n");
                         c2 = s.nextInt();
                         s.nextLine();
                         switch (c2) {
@@ -56,7 +77,7 @@ public class Main_EX3_LM_1_2
                                 System.out.println("enter number of student: ");
                                 int ns = s.nextInt();
                                 s.nextLine();
-                                StudentC[] st = new StudentC[ns];
+                                Student[] st = new Student[ns];
                                 for (int i = 0; i < ns; i++) {
                                     System.out.println("enter name ,family name,id and discipline:");
                                     String name = s.nextLine();
@@ -64,20 +85,41 @@ public class Main_EX3_LM_1_2
                                     long id = s.nextLong();
                                     s.nextLine();
                                     String dis = s.nextLine();
-                                    st[i] = new StudentC(name, familyname, id, dis);
-                                    FileHandle.writeInFile(st[i], "F:/MainProjects/ap/exercises/ex3/Student.txt");
+                                    try
+                                    {
+                                        st[i] = new Student(name, familyname, id, dis);
+                                    }
+                                    catch(InvalidInputException e)
+                                    {
+                                        System.out.println(e.getMessage());
+                                    }
+                                    try
+                                    {
+                                        File f=new File("F:/MainProjects/ap/exercises/ex3/Student.txt");
+                                        ObjectOutputStream O;
+                                        if(f.length()==0)
+                                        {
+                                            O=new ObjectOutputStream(new FileOutputStream("F:/MainProjects/ap/exercises/ex3/Student.txt"));
+                                            O.writeObject(st[i]);
+                                        }
+                                        else
+                                        {
+                                            O=new AppendObj(new FileOutputStream("F:/MainProjects/ap/exercises/ex3/Student.txt",true));
+                                            O.writeObject(st[i]);
+                                        }
+                                    }
+                                    catch(Exception e)
+                                    {
+                                        System.out.println(e.getMessage());
+                                    }
                                 }
-                                break;
-
-                            case 2:
-                                FileHandle.readFileB("F:/MainProjects/ap/exercises/ex3/Student.txt");
                                 break;
 
                             case 3:
                                 System.out.println("exiting...");
 
                         }
-                    } while (c2 != 3);
+                    } while (c2 != 2);
                     break;
 
                 case 3:
@@ -88,198 +130,3 @@ public class Main_EX3_LM_1_2
     }
 }
 
-class BookLib extends Object implements Serializable
-{
-    private String bN;
-    private String aN;
-    private int nP;
-    private int pY;
-    public BookLib()
-    {
-
-    }
-    public BookLib(String bN,String aN,int nP,int pY) throws InvalidInputExceptionErr {
-        if(bN!=null && aN!=null && nP>0 && pY>0)
-        {
-            this.bN = bN;
-            this.aN = aN;
-            this.nP = nP;
-            this.pY = pY;
-        }
-        else {
-            throw new InvalidInputExceptionErr("Error: invalid input!");
-        }
-    }
-
-    public String getBookName()
-    {
-        return bN;
-    }
-    public void setBookName(String bN)
-    {
-        this.bN=bN;
-    }
-
-    public String getAuthorName()
-    {
-        return aN;
-    }
-    public void setAuthorName(String aN)
-    {
-        this.aN=aN;
-    }
-
-    public int getNumPage()
-    {
-        return nP;
-    }
-    public void setNumPage(int nP)
-    {
-        this.nP=nP;
-    }
-
-    public int getPublishedYear()
-    {
-        return pY;
-    }
-    public void setPublishedYear(int pY)
-    {
-        this.pY=pY;
-    }
-
-    @Override
-    public String toString()
-    {
-        return "book name: "+this.bN+"\tauthor name: "+aN+"\t  number of pages: "+nP+"\tyear of published:"+pY+"\n";
-    }
-}
-
-class StudentC extends Object implements Serializable
-{
-    private String n;
-    private String fN;
-    private long id;
-    private String dis;
-
-    public StudentC()
-    {
-
-    }
-
-    public StudentC(String n,String fN,long id,String dis) throws InvalidInputExceptionErr {
-        if(n!=null && fN!=null && id>0 && dis!=null)
-        {
-            this.n=n;
-            this.fN=fN;
-            this.id=id;
-            this.dis=dis;
-        }
-        else
-            throw new InvalidInputExceptionErr("Error: invalid input!");
-    }
-
-    public String getName()
-    {
-        return n;
-    }
-    public void setName(String n)
-    {
-        this.n=n;
-    }
-
-    public String getFamilyName()
-    {
-        return fN;
-    }
-    public void setFamilyName(String fN)
-    {
-        this.fN=fN;
-    }
-
-    public long getid()
-    {
-        return id;
-    }
-    public void setid(long id)
-    {
-        this.id=id;
-    }
-
-    public String getDiscpline()
-    {
-        return dis;
-    }
-    public void setDiscpline(String dis)
-    {
-        this.dis=dis;
-    }
-
-    @Override
-    public String toString()
-    {
-        return "Student name: "+this.n+"\tfamily name: "+fN+"\tid: "+id+"\tdiscpline: "+dis+"\n";
-    }
-}
-
-class FileHandle
-{
-    public static void writeInFile(Object o,String filePath)
-    {
-        try
-        {
-            File f=new File(filePath);
-            ObjectOutputStream O;
-            if(f.length()==0)
-            {
-                O=new ObjectOutputStream(new FileOutputStream(filePath));
-                O.writeObject(o);
-            }
-            else
-            {
-                O=new AppendObj(new FileOutputStream(filePath,true));
-                O.writeObject(o);
-            }
-        }
-        catch(Exception e)
-        {
-            System.out.println(e.getMessage());
-        }
-    }
-    public static void readFileB(String filePath)
-    {
-        try(ObjectInputStream i=new ObjectInputStream(new FileInputStream(filePath)))
-        {
-            while(true)
-            {
-                Object obj=i.readObject();
-                System.out.println(obj.toString());
-            }
-
-        }
-        catch(Exception e)
-        {
-            System.out.println("end of file!");
-        }
-    }
-}
-
-class AppendObj extends ObjectOutputStream
-{
-    public AppendObj(OutputStream out) throws Exception
-    {
-        super(out);
-    }
-    @Override
-    public void writeStreamHeader() throws IOException
-    {
-        reset();
-    }
-}
-
-class InvalidInputExceptionErr extends Exception
-{
-    public InvalidInputExceptionErr(String message)
-    {
-        super(message);
-    }
-}
