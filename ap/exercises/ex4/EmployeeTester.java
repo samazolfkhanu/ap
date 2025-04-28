5a26a1ed6507e84f4b370b476fbdc0f552487368
samazolfkhanu <samazolfkhani0@gmail.com>
2025-04-27 23:55:00 -0700

    Ex4_E3_12

--- ap/exercises/ex4/EmployeeTester.java
+++ ap/exercises/ex4/EmployeeTester.java
@@ -1,2 +1,31 @@
-package ap.exercises.ex4;public class EmployeeTester {
+package ap.exercises.ex4;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class EmployeeTester
+{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        try
+        {
+            System.out.println("enter name and salary: ");
+            String name=s.nextLine();
+            double salary=s.nextDouble();
+            Employee e=new Employee(name,salary);
+            System.out.println("Name: "+e.getName()+"\tSalary: "+e.getSalary());
+            e.raiseSalary(20);
+            System.out.println("Updated Information:\n>>Name: "+e.getName()+"\tSalary: "+e.getSalary());
+
+        }
+
+        catch(InvalidInputException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
