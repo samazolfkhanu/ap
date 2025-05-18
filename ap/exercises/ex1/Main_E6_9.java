6b5af957383fba0fcd0261699ff145db69da9510
samazolfkhanu <samazolfkhani0@gmail.com>
2025-05-18 01:13:00 -0700

    Ex1_E6_9

---
+++ ap/exercises/ex1/Main_E6_9.java
@@ -0,0 +1,21 @@
+package ap.exercises.ex1;
+
+import java.util.Scanner;
+
+public class Main_E6_9 {
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        System.out.print("enter your name: ");
+        String s=scan.nextLine();
+        reversing(s);
+        scan.close();
+    }
+    static void reversing(String s)
+    {
+        for(int i=(s.length())-1;i>=0;i--)
+        {
+            System.out.print(s.charAt(i));
+        }
+    }
+}
