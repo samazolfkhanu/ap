984b17d1d97fa7af78f5aa3f3922e84940fa4a39
samazolfkhanu <samazolfkhani0@gmail.com>
2025-05-18 01:16:17 -0700

    Ex1_E6_16

---
+++ ap/exercises/ex1/Main_E6_16.java
@@ -0,0 +1,16 @@
+package ap.exercises.ex1;
+
+public class Main_E6_16 {
+    public static void main(String[] args)
+    {
+        for(int i=1;i<=10;i++)
+        {
+            for(int j=1;j<=10;j++)
+            {
+                System.out.printf("%d  ",i*j);
+            }
+            System.out.print("\n");
+        }
+    }
+}
+
