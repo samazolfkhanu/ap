0c67379773687bc86e6cf1283a0325eb2122b990
samazolfkhanu <samazolfkhani0@gmail.com>
2025-05-18 03:44:19 -0700

    Ex1_E6_17

---
+++ ap/exercises/ex1/Main_E6_17.java
@@ -0,0 +1,50 @@
+package ap.exercises.ex1;
+
+import java.util.Scanner;
+
+public class Main_E6_17 {
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        System.out.print("enter the length of side:");
+        int s=scan.nextInt();
+        pattern(s);
+    }
+    static void pattern(int n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            System.out.print("*");
+        }
+        System.out.print(" ");
+        for(int i=0;i<n;i++)
+        {
+            System.out.print("*");
+        }
+        System.out.print("\n");
+        for(int i=0;i<n-2;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.print(" *");
+            for(int k=0;k<n-2;k++)
+            {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            System.out.print("\n");
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            System.out.print("*");
+        }
+        System.out.print(" ");
+        for(int i=0;i<n;i++)
+        {
+            System.out.print("*");
+        }
+    }
+}
