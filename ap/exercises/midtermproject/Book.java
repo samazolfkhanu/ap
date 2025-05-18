e28700f2da5b6763f3f8a9fd072129dec1100c83
samazolfkhanu <samazolfkhani0@gmail.com>
2025-05-18 08:29:38 -0700

    mid_project_part2

--- ap/exercises/midtermproject/Book.java
+++ ap/exercises/midtermproject/Book.java
@@ -1,23 +1,26 @@
-package ap.exercises;
+package ap.exercises.midtermproject;
 
 import java.io.Serializable;
 
-public class Book implements Serializable , Comparable<Book>
+public class Book implements Serializable , Comparable<Book>,HashId<Book>
 {
+    private static final long sv=1L;
     private String bN;
     private String aN;
     private int nP;
     private int pY;
+    private Long id;
     private int  borrowCount;
-    public Book(String bN,String aN,int nP,int pY)throws InvalidInputException
+    public Book(String bN,String aN,int nP,int pY,Long id)throws InvalidInputException
     {
-        if(bN!=null && aN!=null && nP>0 && pY>0)
+        if(bN!=null && aN!=null && nP>0 && pY>0 && id>0 )
         {
             this.bN = bN;
             this.aN = aN;
             this.nP = nP;
             this.pY = pY;
             this.borrowCount=0;
+            this.id=id;
         }
         else {
             throw new InvalidInputException("Error: invalid input!");
@@ -82,6 +85,11 @@ public class Book implements Serializable , Comparable<Book>
 
     }
 
+    @Override
+    public Long getId()
+    {
+        return id;
+    }
 
     @Override
     public int compareTo(Book other)
@@ -92,6 +100,6 @@ public class Book implements Serializable , Comparable<Book>
     @Override
     public String toString()
     {
-        return "book name: "+this.bN+"\tauthor name: "+aN+"\tnumber of pages: "+nP+"\tyear of published:"+pY+"\t"+"Number of times loaned"+borrowCount+"\n";
+        return ">>Book Information:"+"\n"+"\tBook Name: "+this.bN+"\n"+"\tAuthor Name: "+aN+"\n"+"\tNumber Of Pages: "+nP+"\n"+"\tYear Of Published:"+pY+"\n"+"\tNumber Of Borrowd Time: "+borrowCount;
     }
 }
