36b533eef9f71ab020cecd9b641d945c76d90096
samazolfkhanu <samazolfkhani0@gmail.com>
2025-05-18 05:01:36 -0700

    EX3_LM_1_1

--- ap/exercises/ex3/Main_EX3_LM_1_1.java
+++ ap/exercises/ex3/Main_EX3_LM_1_1.java
@@ -10,6 +10,7 @@ public class Main_EX3_LM_1_1
             b[0]=new Book("The Gifts Of Imperfection","Brene Brown",138,2010);
             b[1]=new Book("ABC","DEF",349,2006);
 
+            
             Student[] s=new Student[2];
             s[0]=new Student("abc","fff",675432626,"hfgf");
             s[1]=new Student("hfgf","skjd",736863,"dss");
