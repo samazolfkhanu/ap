fa0100a92b1565ab7f91f5ebb42c0e34360c767f
samazolfkhanu <samazolfkhani0@gmail.com>
2025-05-18 03:46:39 -0700

    Ex1_E6_18

---
+++ ap/exercises/ex1/Main_E6_18.java
@@ -0,0 +1,49 @@
+package ap.exercises.ex1;
+
+import java.util.Scanner;
+
+public class Main_E6_18 {
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        System.out.print("enter the side length of rhombus:");
+        int n=scan.nextInt();
+        pattern(n);
+        scan.close();
+    }
+    static void pattern(int n)
+    {
+        int nb=n-1,ns=1;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<nb;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=0;k<ns;k++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+            nb--;
+            ns+=2;
+        }
+        nb=1;
+        ns-=4;
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=0;j<nb;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=0;k<ns;k++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+            nb++;
+            ns-=2;
+        }
+    }
+}
+
