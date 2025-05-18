c84b38c17ca78ceb53e3a91f6901098938dff350
samazolfkhanu <samazolfkhani0@gmail.com>
2025-05-18 01:02:04 -0700

    Ex1_E6_1_e

---
+++ ap/exercises/ex1/Main_E6_1_e.java
@@ -0,0 +1,20 @@
+package ap.exercises.ex1;
+import java.util.Scanner;
+
+public class Main_E6_1_e {
+    public static void main(String[] args)
+    {
+        int m=0,x;
+        Scanner scan=new Scanner(System.in);
+        System.out.print("enter a number:");
+        int n=scan.nextInt();
+        while(n>0)
+        {
+            x=n%10;
+            if(x%2!=0) m=m+x;
+            n=n/10;
+        }
+        System.out.print("sum of odd digits: "+m);
+    }
+}
+
