f4c8e59454e32eeabe6b93c432db95bdaf91d05b
samazolfkhanu <samazolfkhani0@gmail.com>
2025-05-18 00:58:22 -0700

    Ex1_E5_18

---
+++ ap/exercises/ex1/Main_E5_18.java
@@ -0,0 +1,23 @@
+package ap.exercises.ex1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main_E5_18 {
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        System.out.print("enter three strings:");
+        String s=scan.nextLine();
+        String[] s2=s.split(" ");
+        sort(s2);
+    }
+    static void sort(String[] s2)
+    {
+        Arrays.sort(s2);
+        for(int i=0;i<s2.length;i++)
+        {
+            System.out.println(s2[i]);
+        }
+    }
+}
