e8148b84f5e824f7b8c768c6c340bd68bfaa064a
samazolfkhanu <samazolfkhani0@gmail.com>
2025-05-18 01:00:14 -0700

    EX1_E6_1_a

---
+++ ap/exercises/ex1/Main_E6_1_a.java
@@ -0,0 +1,13 @@
+package ap.exercises.ex1;
+
+public class Main_E6_1_a {
+    public static void main(String[] args)
+    {
+        int m=0;
+        for(int i=2;i<=100;i+=2)
+        {
+            m+=i;
+        }
+        System.out.print("sum of even numbers till 100: "+m);
+    }
+}
