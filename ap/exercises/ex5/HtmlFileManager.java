f8097c32beaf0600154f8a016fd70fbdf6448bf6
samazolfkhanu <samazolfkhani0@gmail.com>
2025-06-03 06:40:47 -0700

    EX5

--- ap/exercises/ex5/HtmlFileManager.java
+++ ap/exercises/ex5/HtmlFileManager.java
@@ -32,6 +32,6 @@ public class HtmlFileManager {
 
     public String getSaveHtmlFileAddress(){
         saveCounter++;
-        return saveFileBasePath +"/"+ saveCounter;
+        return saveFileBasePath +"/"+ saveCounter+".html";
     }
 }
