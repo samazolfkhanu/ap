d0f96ea5bb02b7d8ea18d3d2bc81afcbe4c32b75
samazolfkhanu <samazolfkhani0@gmail.com>
2025-06-10 12:01:35 -0700

    EX7

---
+++ ap/exercises/ex7/InvalidInputException.java
@@ -0,0 +1,9 @@
+package ap.exercises.ex7;
+
+public class InvalidInputException extends RuntimeException
+{
+    public InvalidInputException(String message)
+    {
+        super(message);
+    }
+}
