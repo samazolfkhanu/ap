c7b7838739be380b3778267f638b037d6e46e9e0
samazolfkhani-dev <samazolfkhani.work@gmail.com>
2025-08-12 08:28:06 -0700

    FPR_1-1

---
+++ ap/exercises/finalproject/Student.java
@@ -0,0 +1,40 @@
+package ap.exercises.finalproject;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private String name;
+    private String studentId;
+    private String username;
+    private String password;
+
+    public Student(String name, String studentId, String username, String password) {
+        this.name = name;
+        this.studentId = studentId;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name +
+                " | Student ID: " + studentId +
+                " | Username: " + username;
+    }
+}
