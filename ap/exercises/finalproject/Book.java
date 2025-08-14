4d3ea5eac5ab2aa904fbed70df3657e1adfc8735
samazolfkhani-dev <samazolfkhani.work@gmail.com>
2025-08-14 01:40:52 -0700

    FPR_1-4

--- ap/exercises/finalproject/Book.java
+++ ap/exercises/finalproject/Book.java
@@ -1,5 +1,50 @@
 package ap.exercises.finalproject;
 
-public class Book {
+public class Book
+{
+    private String name;
+    private String author;
+    private int publishedYear;
+    private BookState state;
+    public Book(String name,String author,int publishedYear,String state)throws InvalidEntrance
+    {
+        if(!name.isEmpty() && !author.isEmpty() && publishedYear>0 && !state.isEmpty())
+        {
+            this.name=name;
+            this.author=author;
+            this.publishedYear=publishedYear;
+            this.state=BookState.search(state);
+        }
+        else
+        {
+            throw new InvalidEntrance("Invalid Inputs! <100>");
+        }
+    }
+    public String getName()
+    {
+        return this.name;
+    }
+    public String getAuthor()
+    {
+        return this.author;
+    }
+    public int getPublishedYear()
+    {
+        return this.publishedYear;
+    }
+    public String getState()
+    {
+        return this.state.name();
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Book Info:\n" +
+                "Name: "+this.name+
+                " | Author: "+this.author+
+                " | Published Year: "+this.publishedYear+
+                " | State: "+this.state.name();
+    }
 
 }
