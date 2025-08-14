d64757fc5b69e2594bff3598bb6f1285e1360e5a
samazolfkhani-dev <samazolfkhani.work@gmail.com>
2025-08-14 01:39:26 -0700

    FPR_1-4

---
+++ ap/exercises/finalproject/Loan.java
@@ -0,0 +1,72 @@
+package ap.exercises.finalproject;
+
+import java.time.LocalDate;
+
+public class Loan
+{
+    private int idCount=1;
+    private Book book;
+    private Student student;
+    private int id;
+    private LocalDate issueDate;
+    private LocalDate dueDate;
+    private LocalDate returnDate;
+
+    public Loan(Book book,Student student)throws InvalidEntrance
+    {
+        if(book!=null && student!=null)
+        {
+            this.book=book;
+            this.student=student;
+            this.id=idCount;
+            this.dueDate=null;
+            this.issueDate=LocalDate.now();
+            this.returnDate=issueDate.plusDays(30);
+            idCount++;
+        }
+        else
+        {
+            throw new InvalidEntrance("Invalid Inputs! <300>");
+        }
+    }
+
+    public Book getBook()
+    {
+        return book;
+    }
+    public Student getStudent()
+    {
+        return student;
+    }
+    public int getId()
+    {
+        return id;
+    }
+    public String getIssueDate()
+    {
+        return issueDate.toString();
+    }
+    public String getReturnDate()
+    {
+        return returnDate.toString();
+    }
+
+    public void setDueDate()
+    {
+        this.dueDate=LocalDate.now();
+    }
+    public String getDueDate()
+    {
+        return dueDate.toString();
+    }
+
+    public String toString()
+    {
+        return "Loan Info:\n" +
+                "\t"+book+
+                "\n\t"+student+
+                "\nIssueDate: "+issueDate
+                +"\nReturnDate: "+returnDate;
+    }
+
+}
