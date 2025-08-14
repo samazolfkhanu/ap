132f84ec7e7d9a872feb5f827f19ab8597eeca47
samazolfkhani-dev <samazolfkhani.work@gmail.com>
2025-08-14 02:23:34 -0700

    FPR_1-3

--- ap/exercises/finalproject/BookHandler.java
+++ ap/exercises/finalproject/BookHandler.java
@@ -14,44 +14,85 @@ public class BookHandler
         f=new FileHandling<>("F:/JavaProject/ap/exercises/finalproject/Books.txt");
     }
 
-    public void displayAvailableBooks()
+    public List<Book> getBooks()
     {
         if(!books.isEmpty())
             books.clear();
-        books=f.readFromFile(Book.class);
-        if(!books.isEmpty())
-        {
-            for(Book book:books)
-                System.out.println(book);
-        }
-        else
-        {
-            System.out.println("No Book!");
-        }
+        return books=f.readFromFile(Book.class);
     }
-
-    public Book isBookAvailable(String name,String author,int publishedYear)
-    {
-        if(!books.isEmpty())
-            books.clear();
-        books=f.readFromFile(Book.class);
-        for(Book book:books)
-        {
-            if(book.getName().equalsIgnoreCase(name) && book.getAuthor().equalsIgnoreCase(author) && book.getPublishedYear()==publishedYear)
-                if(book.getState().equalsIgnoreCase("AVAILABLE")) {
-                    book.setState1();
-                    updateFile(books);
-                    return book;
-                }
-        }
-        return null;
-    }
-
     public void updateFile(List<Book> book)
     {
         for(Book b:book)
         {
             f.writeInFile(b);
+        }
+    }
+    
+    public void displayAvailableBooks()
+    {
+        getBooks();
+        if(!books.isEmpty())
+        {
+            for(Book book:books)
+            {
+                if(book.getState().equalsIgnoreCase("AVAILABLE"))
+                    System.out.println(book);
+            }
+        }
+        else
+        {
+            throw new BookException("No Book In List!");
+        }
+    }
+
+    public Book isBookAvailable(String name,String author,int publishedYear)
+    {
+        getBooks();
+        if(!books.isEmpty())
+        {
+            for(Book book:books)
+            {
+                if(book.getName().equalsIgnoreCase(name) &&
+                        book.getAuthor().equalsIgnoreCase(author) &&
+                        book.getPublishedYear()==publishedYear)
+
+                    if(book.getState().equalsIgnoreCase("AVAILABLE")) {
+                        book.setState1();
+                        updateFile(books);
+                        return book;
+                    }
+            }
+        }
+        else
+        {
+            throw new BookException("No Book In List!");
+        }
+        System.out.println("Book Not Found!");
+        return null;
+    }
+
+    public void searchBook(String name,String author,int publishedYear)
+    {
+        getBooks();
+        if(!books.isEmpty())
+        {
+            boolean isFound=false;
+            for(Book book:books)
+            {
+                if(book.getName().equalsIgnoreCase(name) &&
+                        book.getAuthor().equalsIgnoreCase(author) &&
+                        book.getPublishedYear()==publishedYear) {
+                    isFound=true;
+                    System.out.println(book);
+                    break;
+                }
+            }
+            if(!isFound)
+                System.out.println("Book Not Found!");
+        }
+        else
+        {
+            throw new BookException("No Book In List!");
         }
     }
 }
