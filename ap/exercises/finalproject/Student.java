f745c56e0079222efc54267dc5b91d4fe1dc2c3a
samazolfkhani-dev <samazolfkhani.work@gmail.com>
2025-08-18 23:15:54 -0700

    FPR_3-7

--- ap/exercises/finalproject/Student.java
+++ ap/exercises/finalproject/Student.java
@@ -2,17 +2,19 @@ package ap.exercises.finalproject;
 
 import java.io.Serializable;
 
-public class Student implements Serializable,User {
+public class Student implements Serializable {
     private String name;
     private String studentId;
     private String username;
     private String password;
+    private AccessLevel permission;
 
     public Student(String name, String studentId, String username, String password) {
         this.name = name;
         this.studentId = studentId;
         this.username = username;
         this.password = password;
+        this.permission= AccessLevel.search("active");
     }
 
     public String getName() {
@@ -31,6 +33,17 @@ public class Student implements Serializable,User {
         return password;
     }
 
+    public String getPermission()
+    {
+        return permission.name();
+    }
+
+    public void setPermission(String p) throws InvalidEntrance {
+        if(p!=null)
+            this.permission= AccessLevel.search(p);
+        else
+            throw new InvalidEntrance("Invalid Permission Input!");
+    }
     @Override
     public String toString() {
         return "Student Info:\n" +
