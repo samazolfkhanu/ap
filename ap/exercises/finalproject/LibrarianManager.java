9420899b31b410ead9a5aef969726f690e774837
samazolfkhani-dev <samazolfkhani.work@gmail.com>
2025-09-12 08:40:39 -0700

    FPR_4-4

--- ap/exercises/finalproject/LibrarianManager.java
+++ ap/exercises/finalproject/LibrarianManager.java
@@ -1,34 +1,42 @@
 package ap.exercises.finalproject;
 
-import java.util.ArrayList;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class LibrarianManager implements List_Tool<Librarian>, Account_Handler<Librarian>
 {
-    private List<Librarian> librarians;
+    private Map<String,Librarian> librarians;
     private FileHandling<Librarian> lF;
 
     public LibrarianManager()
     {
-        librarians=new ArrayList<>();
+        librarians=new HashMap<>();
         lF=new FileHandling<>("F:/JavaProject/ap/exercises/finalproject/Librarians.json");
     }
     public Librarian authenticateUser(String username,String password) {
         getList();
-        return librarians.stream()
+        return librarians.values().stream()
                 .filter(l->l.getUsername().equals(username) && l.getPassword().equals(password))
                 .findFirst()
                 .orElse(null);
     }
 
     public void getList() {
+        List<Librarian> l;
         if(librarians!=null && !librarians.isEmpty())
             librarians.clear();
-        librarians=lF.readFromFile(Librarian.class);
+        l=lF.readFromFile(Librarian.class);
+        if(l!=null) {
+            librarians=l.stream()
+                    .collect(Collectors.toMap(Librarian::getUsername,x->x));
+        }
     }
     public void editLibrarianInformation(Librarian librarian,String password) throws InvalidEntrance {
         getList();
-        for(Librarian l:librarians) {
+        for(Librarian l:librarians.values()) {
             if(librarian.getUsername().equals(l.getUsername()) && librarian.getPassword().equals(l.getPassword())) {
                 l.setPassword(password);
             }
@@ -37,7 +45,7 @@ public class LibrarianManager implements List_Tool<Librarian>, Account_Handler<L
     }
     public void increaseLibrarianBook(String username){
         getList();
-        for(Librarian l:librarians) {
+        for(Librarian l:librarians.values()) {
             if(l.getUsername().equals(username)) {
                 l.increaseNumberOfBook();
                 break;
@@ -45,9 +53,9 @@ public class LibrarianManager implements List_Tool<Librarian>, Account_Handler<L
         }
         updateList(librarians);
     }
-    public void updateList(List<Librarian> l) {
+    public void updateList(Map<String,Librarian> l) {
         lF.clearFile();
-        for(Librarian librarian:l) {
+        for(Librarian librarian:l.values()) {
             lF.writeInFile(librarian,Librarian.class);
         }
     }
@@ -55,14 +63,15 @@ public class LibrarianManager implements List_Tool<Librarian>, Account_Handler<L
         getList();
         if(librarians!=null)
         {
-            return librarians.stream()
-                    .filter(x->x.getUsername().equals(username))
+            return librarians.entrySet().stream()
+                    .filter(x->x.getKey().equals(username))
                     .findFirst()
-                    .get();
+                    .get()
+                    .getValue();
         }
         return null;
     }
-    public List<Librarian> returnLibrarian() {
+    public Map<String,Librarian> returnLibrarian() {
         getList();
         return librarians;
     }
@@ -70,10 +79,11 @@ public class LibrarianManager implements List_Tool<Librarian>, Account_Handler<L
     {
         getList();
         if(librarians!=null) {
-            Librarian l = librarians.stream()
-                    .filter(x -> x.getUsername().equals(username))
+            Librarian l = librarians.entrySet().stream()
+                    .filter(x -> x.getKey().equals(username))
                     .findFirst()
-                    .get();
+                    .get()
+                    .getValue();
             System.out.println("Number of Book Registered: " + l.getNumBook());
         }
     }
@@ -82,13 +92,13 @@ public class LibrarianManager implements List_Tool<Librarian>, Account_Handler<L
     {
         getList();
         if(librarians!=null)
-            return librarians.stream().anyMatch(s->s.getUsername().equalsIgnoreCase(username));
+            return librarians.entrySet().stream().anyMatch(s->s.getKey().equalsIgnoreCase(username));
         return false;
     }
     public void addLibrarian(String username,String id) throws InvalidEntrance {
         Librarian lib=new Librarian(username.trim(),id);
         getList();
-        if(librarians!=null && librarians.contains(lib))
+        if(librarians!=null && librarians.containsKey(lib))
             System.out.println("Librarian Has Already Added!");
         if(isUsernameTaken(username))
             System.out.println("This username already exists. Please choose a different username.");
